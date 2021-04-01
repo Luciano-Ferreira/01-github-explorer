dfc9138b4aa2f3f32445555f0b558b509fef87c3
Luciano Silva <luciano2023silva@gmail.com>
2021-04-01 12:47:05 -0300

    remove counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { RepositoryList } from '../components/RepositoryList';
-import { Counter } from '../components/Counter';
 import './styles/global.scss';
 
 export function App() {
     return(
-        <>
             <RepositoryList />
-            <Counter />
-        </>
     )
 }
