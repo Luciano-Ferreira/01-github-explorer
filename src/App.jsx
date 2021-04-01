8d15197e6df0814913067b42b0ee59d3ad680c64
Luciano Silva <luciano2023silva@gmail.com>
2021-04-01 13:11:16 -0300

    styles of repositorylist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RepositoryList } from '../components/RepositoryList';
+import { RepositoryList } from '../src/components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
