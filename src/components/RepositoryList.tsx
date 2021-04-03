ddd43740fc8ebfd7f499ffa3094b9899e4b768e1
Luciano Silva <luciano2023silva@gmail.com>
2021-04-03 18:24:11 -0300

    migrate to typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss'
 
+interface IRepository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<IRepository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/users/luciano-ferreira/repos')
